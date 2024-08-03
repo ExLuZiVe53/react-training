57e6f3c0435f51116ed770be52a5e342e5974489
ExLuZiVe53 <imstudent27@gmail.com>
2024-08-03 17:46:52 +0200

    added clsx library and view is it

--- src/components/PaymentCard/PaymentCard.jsx
+++ src/components/PaymentCard/PaymentCard.jsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import css from "./PaymentCard.module.css";
 
 export default function PaymentCard({
@@ -12,12 +13,46 @@ export default function PaymentCard({
     isPaid,
   },
 }) {
+  // console.log(clsx(css.text, isPaid ? css.isPaid : css.isPending));
+  // console.log(clsx("a", null && "b", false && "c"));
+  // console.log(
+  //   clsx("a", "x", {
+  //     b: null,
+  //     c: true,
+  //     d: false,
+  //   })
+  // );
+  // console.log(
+  //   clsx("text", {
+  //     isPaid: isPaid,
+  //     isPending: !isPaid,
+  //   })
+  // );
+  // console.log(
+  //   clsx(css.text, {
+  //     [css.isPaid]: isPaid,
+  //     [css.isPending]: !isPaid,
+  //   })
+  // );
+  // const textClasses = [css.text];
+  // if (isPaid) {
+  //   textClasses.push(css.isPaid);
+  // } else {
+  //   textClasses.push(css.isPending);
+  // }
+  // console.log(textClasses.join(" "));
+
+  const amountClassNames = clsx(css.text, isPaid ? css.isPaid : css.isPending);
+
   return (
     <div className={css.container}>
       <p className={css.text}>Amount: {amount}</p>
       {/* {isPaid ? <p>Status: Paid</p> : <p>Status: Pending</p>} */}
       {/* <p>{isPaid ? "Status: Paid" : "Status: Pending"}</p> */}
-      <p className={css.text}>Status: {isPaid ? "Paid" : "Pending"}</p>
+      {/* <p className={textClasses.join(" ")}>
+        Status: {isPaid ? "Paid" : "Pending"}
+      </p> */}
+      <p className={amountClassNames}>Status: {isPaid ? "Paid" : "Pending"}</p>
       <p className={css.text}>Description: {description}</p>
       <p className={css.text}>Card Number: {cardNumber}</p>
       <p className={css.text}>Card Type: {cardType}</p>

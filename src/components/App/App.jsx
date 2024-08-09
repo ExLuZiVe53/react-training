b99da434964508e8371502efb2926dc8d793eda1
ExLuZiVe53 <imstudent27@gmail.com>
2024-08-09 20:40:33 +0200

    added events

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,26 @@
 import css from "./App.module.css";
 
 export default function App() {
-  const handleClick = () => {
+  const handleClick = (message) => {
+    console.log(message);
     console.log("Hello");
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("You submit form");
   };
 
   return (
     <div className={css.container}>
       <h1>State in React</h1>
       <hr />
+      <button onClick={() => handleClick("this is Arg")}>Click me!</button>
       <button onClick={handleClick}>Click me!</button>
+
+      <form onSubmit={handleSubmit}>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }

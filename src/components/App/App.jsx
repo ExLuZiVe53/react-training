bb1b7a8eadd6a01503a2b38c3e55880e527b9b66
ExLuZiVe53 <imstudent27@gmail.com>
2024-08-13 20:58:21 +0200

    added state and setter from state(function from change state)

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,26 +1,31 @@
+import { useState } from "react";
 import css from "./App.module.css";
 
 export default function App() {
-  const handleClick = (message) => {
-    console.log(message);
-    console.log("Hello");
+  const [clicks, setClicks] = useState(0);
+  // const [text, setText] = useState("Hello");
+  const [a, setA] = useState(0);
+
+  const handleA = () => {
+    setA(a + 1);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log("You submit form");
+  const handleClick = () => {
+    setClicks(clicks + 1);
   };
+
+  // const handleChange = (event) => {
+  //   setText(event.target.value);
+  // };
 
   return (
     <div className={css.container}>
       <h1>State in React</h1>
       <hr />
-      <button onClick={() => handleClick("this is Arg")}>Click me!</button>
-      <button onClick={handleClick}>Click me!</button>
-
-      <form onSubmit={handleSubmit}>
-        <button>Submit</button>
-      </form>
+      <button onClick={handleClick}>Number of clicks: {clicks}</button>
+      <button onClick={handleA}>{a}</button>
+      {/* <input type="text" onChange={handleChange} />
+      <p>{text}</p> */}
     </div>
   );
 }

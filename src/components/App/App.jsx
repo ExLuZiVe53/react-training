f418765f26ff9bb6e8e03b3352495332dea2e242
ExLuZiVe53 <imstudent27@gmail.com>
2024-08-15 15:16:53 +0200

    create toogler with text

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,32 @@
 import { useState } from "react";
-// import ClickCounter from "../ClickCounter/ClickCounter";
+import ClickCounter from "../ClickCounter/ClickCounter";
+import Toogler from "../Toogler/Toogler";
 import css from "./App.module.css";
 
 export default function App() {
-  // const [clicks, setClicks] = useState(0);
+  const [clicks, setClicks] = useState(0);
 
-  const [obj, setObj] = useState({ a: 0, b: 0, c: { x: 5, y: 10 } });
+  // const [obj, setObj] = useState({ a: 0, b: 0, c: { x: 5, y: 10 } });
 
-  const handleChangeObj = () => {
-    setObj({ ...obj, c: { ...obj.c, x: 50 } });
-  };
-
-  // const handleClick = () => {
-  //   setClicks(clicks + 1);
+  // const handleChangeObj = () => {
+  //   setObj({ ...obj, c: { ...obj.c, x: 50 } });
   // };
+
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
   return (
     <div className={css.container}>
       <h1>State in React</h1>
       <hr />
-      <button onClick={handleChangeObj}>Change obj state</button>
-      {/* <ClickCounter onUpdate={handleClick} value={clicks} />
+      {/* <button onClick={handleChangeObj}>Change obj state</button> */}
       <ClickCounter onUpdate={handleClick} value={clicks} />
-      <ClickCounter onUpdate={handleClick} value={clicks} /> */}
+      <ClickCounter onUpdate={handleClick} value={clicks} />
+      <ClickCounter onUpdate={handleClick} value={clicks} />
+      <hr />
+      <Toogler />
+      <Toogler />
+      <Toogler />
     </div>
   );
 }

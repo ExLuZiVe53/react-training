7d8192776e62b523c8669247ade261e1a95b8163
ExLuZiVe53 <imstudent27@gmail.com>
2024-08-19 20:08:46 +0200

    we present the code in order and finished dynamics

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,48 +1,31 @@
+import { useState } from "react";
+
 import Reader from "../Reader/Reader";
+import ClickCounter from "../ClickCounter/ClickCounter";
+import Toogler from "../Toogler/Toogler";
 import articles from "../../articles.json";
 import css from "./App.module.css";
 
 export default function App() {
-  // console.log(data);
+  const [clicks, setClicks] = useState(0);
+
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
+
   return (
     <div className={css.container}>
       <h1>State in React</h1>
       <hr />
       <Reader items={articles} />
       <hr />
+      <ClickCounter onUpdate={handleClick} value={clicks} />
+      <ClickCounter onUpdate={handleClick} value={clicks} />
+      <ClickCounter onUpdate={handleClick} value={clicks} />
+      <hr />
+      <Toogler />
+      <Toogler />
+      <Toogler />
     </div>
   );
 }
-
-// import { useState } from "react";
-// import ClickCounter from "../ClickCounter/ClickCounter";
-// import Toogler from "../Toogler/Toogler";
-// import css from "./App.module.css";
-
-// export default function App() {
-//   const [clicks, setClicks] = useState(0);
-
-//   const [obj, setObj] = useState({ a: 0, b: 0, c: { x: 5, y: 10 } });
-
-//   const handleChangeObj = () => {
-//     setObj({ ...obj, c: { ...obj.c, x: 50 } });
-//   };
-
-//   const handleClick = () => {
-//     setClicks(clicks + 1);
-//   };
-//   return (
-//     <div className={css.container}>
-//       <h1>State in React</h1>
-//       <hr />
-//       {/* <button onClick={handleChangeObj}>Change obj state</button> */}
-//       <ClickCounter onUpdate={handleClick} value={clicks} />
-//       <ClickCounter onUpdate={handleClick} value={clicks} />
-//       <ClickCounter onUpdate={handleClick} value={clicks} />
-//       <hr />
-//       <Toogler />
-//       <Toogler />
-//       <Toogler />
-//     </div>
-//   );
-// }
